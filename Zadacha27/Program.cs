4884d3f9ab270234269185bd69f0fac9ca14ad60
Anna <eekolegov@mail.ru>
2022-09-29 20:08:15 +0300

    Zadacha27

---
+++ Zadacha27/Program.cs
@@ -0,0 +1,16 @@
+﻿void Zadacha27()
+{
+    Console.WriteLine("Введите число");
+    int a=Convert.ToInt32(Console.ReadLine());
+    int b=a;
+    int sum=0;
+    while (a>0)
+    {
+        sum=sum+a%10;
+        a=a/10;
+
+    }
+    Console.WriteLine($"Сумма цифр числа {b} равна {sum}");
+    
+}
+Zadacha27();
