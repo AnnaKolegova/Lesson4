885c75433cce0cc173190c155360e45a3f91b52d
Anna <eekolegov@mail.ru>
2022-09-29 19:58:53 +0300

    Zadacha25

---
+++ Zadacha25/Program.cs
@@ -0,0 +1,15 @@
+﻿void Zadacha25()
+{
+    Console.WriteLine("Введите число");
+    int a=Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите степень числа");
+    int b=Convert.ToInt32(Console.ReadLine());
+    int pow=1;
+    for (int i=0; i<b; i++)
+    {
+        pow=pow*a;
+    }
+    Console.WriteLine($"Число {a} в степени {b} равно {pow}");
+
+}
+Zadacha25();
